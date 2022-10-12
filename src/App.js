26afac992d7dc38712762a0805bbc16963ee69e9
Ugonz86 <ugonzalez86@gmail.com>
2022-10-11 18:28:53 -0700

    Initial commit. First phase of the website created.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import kklogo2 from "./kklogo2.png";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+        <div className="container">
+          <div className="spinner"></div>
+          <img src={kklogo2} className="App-logo" alt="logo" />
+        </div>
+        <p>For app support, please contact us at kkappdevtester@gmail.com</p>
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
+      <footer className="footer">
+        App and Website Developed by UGX
+      </footer>
     </div>
   );
 }
